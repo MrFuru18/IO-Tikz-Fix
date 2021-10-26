414f823cb938c7d98d4d2fec4542f0167fec411b
Wojciech Babiński <wojcbab281@student.polsl.pl>
2021-10-26 15:42:29 +0200

    setting up the application
    testing Canvas control
    line can be drawn by selecting two points on the canvas
    displaying coordnates in bottom left corner

--- Tikz Fix/MainWindow.xaml.cs
+++ Tikz Fix/MainWindow.xaml.cs
@@ -20,9 +20,50 @@ namespace Tikz_Fix
     /// </summary>
     public partial class MainWindow : Window
     {
+        //private List<Point> points = new List<Point>();
+        Point oldPoint = new Point();
+        int index = 0;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Surface_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ButtonState == MouseButtonState.Pressed)
+            {
+                Ellipse ellipse = new Ellipse();
+
+                ellipse.Stroke = Brushes.Black;
+                ellipse.Width = 5;
+                ellipse.Height = 5;
+                ellipse.Fill = Brushes.Black;
+                double left = e.GetPosition(this).X - (ellipse.Width / 2);
+                double top = e.GetPosition(this).Y - (ellipse.Height / 2);
+
+                ellipse.Margin = new Thickness(left, top, 0, 0);
+                Surface.Children.Add(ellipse);
+
+                if (index >= 1)
+                {
+                    Line line = new Line();
+
+                    line.Stroke = Brushes.Black;
+                    line.X1 = oldPoint.X;
+                    line.Y1 = oldPoint.Y;
+                    line.X2 = e.GetPosition(this).X;
+                    line.Y2 = e.GetPosition(this).Y;
+
+                    Surface.Children.Add(line);
+                    index = -1;
+
+                    TikzCode.Text = "Line (" + line.X1 + "," + line.Y1 + ") , (" + line.X2 + "," + line.Y2 + ")";
+
+                }
+                oldPoint = e.GetPosition(this);
+                index++;
+            }
+                
         }
     }
 }
