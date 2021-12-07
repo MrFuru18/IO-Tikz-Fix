bd0d6483cb662ce2b56830c3e6930e3c67505952
Wojciech Babiński <wojcbab281@student.polsl.pl>
2021-12-07 15:01:05 +0100

    MVVM Structure

---
+++ Tikz Fix/ViewModel/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tikz_Fix.ViewModel.BaseClass;
+
+namespace Tikz_Fix.ViewModel
+{
+    class MainViewModel : BaseViewModel
+    {
+        public MainViewModel()
+        {
+            
+        }
+
+    }
+}
